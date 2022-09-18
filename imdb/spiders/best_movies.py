603b6a96a7eaded8eb7cd05dec1429c239d77a31
Oussema Sferi <oussema.sferi@gmail.com>
2022-09-18 01:46:34 +0100

    handling all pages

--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -12,6 +12,7 @@ class BestMoviesSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[2]"))
     )
 
     def parse_item(self, response):
